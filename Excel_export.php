c2f497bce8832b7492d42f3245a79c035b5e3a89
Adrian Hazael Muñoz Martinez <hijodeoz.93@gmail.com>
2020-07-02 21:25:50 -0500

    Se acabo esta cosa, ya diosito ya no mas chamba XD

--- Excel_export.php
+++ Excel_export.php
@@ -5,7 +5,7 @@ $con=Connect::conectar();
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $ruta='archivos/';
-$datos=$con->query("SELECT idAlumno,Nombre_A,Telefono_A,Correo_A,Carrera,Nombre_D,escuelas.Nombre as insti,delegaciones.Nombre as alcaldia FROM alumno 
+$datos=$con->query("SELECT idAlumno,Nombre_A,Apellido,Telefono_A,Correo_A,Carrera,Nombre_D,escuelas.Nombre as insti,delegaciones.Nombre as alcaldia FROM alumno 
 INNER JOIN docente on alumno.Id_Docente=docente.ID_Docente INNER JOIN escuelas on alumno.IdEscuela=escuelas.IdEscuela 
 INNER JOIN delegaciones on escuelas.IdDelegaciones=delegaciones.IdDelegacion") or die ($con->error);
 $documento=new Spreadsheet();
@@ -15,17 +15,18 @@ $reader=\PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
 
 $hoja=$documento->getActiveSheet();
 $hoja->setTitle('Prueba de hoja');
-$encbezados=["Id Alumno","Nombre","Telefono", "Correo","Carrera","Nombre del docente", "Institucion", "Alcaldia"];
+$encbezados=["Id Alumno","Nombre","Apellidos","Telefono", "Correo","Carrera","Nombre del docente", "Institucion", "Alcaldia"];
 $hoja->fromArray($encbezados,null,'A1');
 while ($row=$datos->fetch_assoc()){
     $documento->getActiveSheet()->SetCellValue('A'.$fila, $row['idAlumno']);
     $documento->getActiveSheet()->SetCellValue('B'.$fila, $row['Nombre_A']);
-    $documento->getActiveSheet()->SetCellValue('C'.$fila, $row['Telefono_A']);
-    $documento->getActiveSheet()->SetCellValue('D'.$fila, $row['Correo_A']);
-    $documento->getActiveSheet()->SetCellValue('E'.$fila, $row['Carrera']);
-    $documento->getActiveSheet()->SetCellValue('F'.$fila, $row['Nombre_D']);
-    $documento->getActiveSheet()->SetCellValue('G'.$fila, $row['insti']);
-    $documento->getActiveSheet()->SetCellValue('H'.$fila, $row['alcaldia']);
+    $documento->getActiveSheet()->SetCellValue('C'.$fila, $row['Apellido']);
+    $documento->getActiveSheet()->SetCellValue('D'.$fila, $row['Telefono_A']);
+    $documento->getActiveSheet()->SetCellValue('E'.$fila, $row['Correo_A']);
+    $documento->getActiveSheet()->SetCellValue('F'.$fila, $row['Carrera']);
+    $documento->getActiveSheet()->SetCellValue('G'.$fila, $row['Nombre_D']);
+    $documento->getActiveSheet()->SetCellValue('H'.$fila, $row['insti']);
+    $documento->getActiveSheet()->SetCellValue('I'.$fila, $row['alcaldia']);
     $fila++;
 }
 $writter=new Xlsx($documento);
